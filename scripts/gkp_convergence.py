d1a400ba57885ed1ca9a014e1f1f0411dc0a63f6
mgarnier59 <50111289+mgarnier59@users.noreply.github.com>
2025-10-23 16:59:18 +0200

    add small convergence script

---
+++ scripts/gkp_convergence.py
@@ -0,0 +1,32 @@
+"""example script for testing the convergence of an
+approximate GKP state stellar fidelity with the number of
+optimisation iteration
+"""
+
+from stellar.cvstates import GKPState
+from stellar.profile import compute_sup_fidelity
+
+
+if __name__ == "__main__":
+
+    tgt_state = GKPState()
+
+    rank = 3
+
+    results = []
+
+    # other parameters for convergence analysis:
+    #   - `tol` parameter of the GKP state (how many Gaussians in the decomposition)
+    #   - starting point `x0` of the optimization
+    #   - ...
+
+    print("Starting...")
+    for n in range(100, 600, 100):
+
+        res = compute_sup_fidelity(max_rank=rank, target_state=tgt_state, method="g", niter=n)
+        results.append(-res.fun)
+
+    print("Finished!")
+    print(f"The results are: {results=}.")
+
+
