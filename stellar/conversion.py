a4e874b3a202fc12d92c00798c730262b8c07b27
mgarnier59 <50111289+mgarnier59@users.noreply.github.com>
2025-12-16 17:22:16 +0100

    init push conversion

--- stellar/conversion.py
+++ stellar/conversion.py
@@ -1,7 +1,7 @@
 from enum import Enum, auto
 import logging
 from math import floor
-from typing import TypeVar
+from typing import TypeVar, assert_never
 from stellar.cvstates import HermitianCVOp, PureCVState
 from stellar.profile import StellarProfile
 
@@ -39,6 +39,14 @@ def max_trace_distance_precision(
             return max_trace_distance_precision_pure_pure_det(
                 from_profile=from_profile, to_profile=to_profile, nb_copies=nb_copies
             )
+        elif protocol is Protocol.probabilistic:
+            pass
+            # return max_trace_distance_precision_pure_pure_prob(
+            #     from_profile=from_profile, to_profile=to_profile, nb_copies=nb_copies
+            # )
+        else:
+            assert_never(protocol)
+
     else:
         raise NotImplementedError
 
@@ -54,12 +62,38 @@ V = TypeVar("V", bound=PureCVState)
 
 def max_trace_distance_precision_pure_pure_det(
     from_profile: StellarProfile[U], to_profile: StellarProfile[V], nb_copies: int
-) -> float:  # or None, error
-    """finding max trace distance for deterministic conversion between pure states with a fixed number of copies"""
-    logger.info("Starting Gaussian conversion analysis...")
+) -> float:
+    """finding max trace distance for deterministic conversion between pure states with a fixed number of copies.
+    Eq. (34) [HFFC25]"""
+    logger.info("Starting deterministicGaussian conversion analysis...")
     # avoid recomputing this
     max_rank_from = max(from_profile.profile.keys())
     max_rank_to = max(to_profile.profile.keys())
+
+    max_n = min([max_rank_from, max_rank_to // nb_copies])  # floor taken by integer division
+
+    print(f"{max_n=}")
+    distance_list: list[float] = []
+
+    # do we need to know the n giving the max?
+    for n in range(0, max_n + 1):
+        distance_list.append(1 - to_profile.profile[nb_copies * n] - nb_copies * (1 - from_profile.profile[n]))
+    print(f"{distance_list=}")
+    return max(distance_list)
+
+
+def max_trace_distance_precision_pure_pure_prob(
+    from_profile: StellarProfile[U], to_profile: StellarProfile[V], nb_copies: int
+) -> float:  # or None, error
+    """finding max trace distance for probabilistic conversion between pure states with a fixed number of copies.
+    The actual profile of the target state is not required since it is a looser bound see Eq. (35) [HFFC25]."""
+    logger.info("Starting probabilistic Gaussian conversion analysis...")
+    # avoid recomputing this
+    max_rank_from = max(from_profile.profile.keys())
+    max_rank_to = max(to_profile.profile.keys())
+
+    # need to supply the rank of input
+    # change parameters
 
     max_n = min([max_rank_from, max_rank_to // nb_copies])  # floor taken by integer division
 
--- tests/test_conversion.py
+++ tests/test_conversion.py
@@ -1,5 +1,7 @@
 """test conversion module"""
 
+import json
+import time
 from stellar.conversion import max_trace_distance_precision_pure_pure_det
 from stellar.cvstates import CatState, CoherentState, GKPState
 from stellar.params import Method, OptimisationParameters
@@ -18,21 +20,33 @@ def test_det_conversion() -> None:
     from_profile = compute_profile(ranks=list(range(max_rank)), target_state=from_state, optim_params=pars)
     to_profile = compute_profile(ranks=list(range(max_rank)), target_state=to_state, optim_params=pars)
     max_dist = max_trace_distance_precision_pure_pure_det(from_profile=from_profile, to_profile=to_profile, nb_copies=2)
-    print(f"{max_dist=}") # need 1 - f_0(cat)
+    print(f"{max_dist=}")  # need 1 - f_0(cat)
     # assert False
 
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_det_conversion_paper() -> None:
     from_state = CatState(amplitude=1, parity=True)  # 6
     to_state = GKPState(delta=0.3, kappa=0.3)  # more terms (11 vs 4) than Δ = κ = 0.3
 
-    max_rank = 5
+    max_rank = 1  # why 20 profile file?
     # same parameters for both states
     pars = OptimisationParameters(method=Method.gaussian, niter=350)
-
+    init = time.time()
     from_profile = compute_profile(ranks=list(range(max_rank)), target_state=from_state, optim_params=pars)
+    from_time = time.time()
+    print(f"from profile time {from_time - init}")
     to_profile = compute_profile(ranks=list(range(max_rank)), target_state=to_state, optim_params=pars)
+    print(f"to_profile {to_profile.profile}")
+    to_time = time.time()
+    print(f"to profile time {to_time - from_time}")
+
+    from_profile.save_to_file("test")
+    to_profile.save_to_file("gkp33350")
+    # with open("gkp33350" + ".json", "w") as f:
+    #     json.dump(to_profile.profile, f)
+    dist_time = time.time()
     max_dist = max_trace_distance_precision_pure_pure_det(from_profile=from_profile, to_profile=to_profile, nb_copies=1)
     print(f"{max_dist=}")  # expect something around 0.23
+    print(f"dist time {dist_time - to_time}")
     assert False
